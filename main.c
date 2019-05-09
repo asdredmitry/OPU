f02a98074f2d03be9d66ab7f4c63ca319bddebd2
asdre <michalych2014@yandex.ru>
2019-05-10 00:41:45 +0300

    all

---
+++ main.c
@@ -0,0 +1,409 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+const double EPS = 1e-50;
+const double H_MIN =  __DBL_MAX__;
+
+const unsigned int FULL_NORM = 1;
+const unsigned int STEPS_COUNT = 1 << 1;
+const unsigned int LAST_NORM = 1 << 2;
+const unsigned int NO_WRITE = 1 << 3;
+const unsigned int GET_LAST = 1 << 5;
+const unsigned int PRINT_H = 1 << 6;
+const unsigned int PRINT_GLOBAL_ERROR = 1 << 7;
+
+const unsigned int WRITE_PERIODS = 1;
+const unsigned int WRITE_ZEROES = 1 << 2;
+
+
+const double fac = 0.8;
+const double facmin = 0.0;
+const double facmax = 1.5;
+
+const double alpha = 0.;
+
+double c[13] = {0., 1./18., 1./12., 1./8., 5./16., 3./8., 59./400., 93./200., 5490023248./9719169821., 13./20., 1201146811./1299019798., 1., 1.};
+double a[13][12] = {{0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.},\
+                    {1./18., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.},\
+                    {1./48., 1./16., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.},\
+                    {1./32., 0., 3./32., 0., 0., 0., 0., 0., 0., 0., 0., 0.},\
+                    {5./16., 0., -75./64, 75./64., 0., 0., 0., 0., 0., 0., 0., 0.},\
+                    {3./80., 0., 0., 3./16., 3./20., 0., 0., 0., 0., 0., 0., 0.},\
+                    {29443841./614563906., 0., 0., 77736538./692538347., -28693883./1125000000., 23124283./1800000000., 0., 0., 0., 0., 0., 0.},\
+                    {16016141./946692911., 0., 0., 61564180./158732637., 22789713./633445777., 545815736./2771057229., -180193667./1043307555., 0., 0., 0., 0., 0.},\
+                    {39632708./573591083., 0., 0., -433636366./683701615., -421739975./2616292301., 100302831./723423059., 790204164./839813087., 800635310./3783071287., 0., 0., 0., 0.},\
+                    {246121993./1340847787., 0., 0., -37695042795./15268766246., -309121744./1061227803., -12992083./490766935., 6005943493./2108947869., 393006217./1396673457., 123872331./1001029789., 0., 0., 0.},\
+                    {-1028468189./846180014., 0., 0., 8478235783./508512852., 1311729495./1432422823., -10304129995./1701304382., -48777925059./3047939560., 15336726248./1032824649., -45442868181./3398467696., 3065993473./597172653., 0., 0.},\
+                    {185892177./718116043., 0., 0., -3185094517./667107341., -477755414./1098053517., -703635378./230739211., 5731566787./1027545527, 5232866602./850066563., -4093664535./808688257., 3962137247./1805957418., 65686358./487910083., 0},\
+                    {403863854./491063109., 0., 0., -5068492393./434740067., -411421997./543043805., 652783627./914296604., 11173962825./925320556., -13158990841./6184727034., 3936647629./1978049680., -160528059./685178525., 248638103./1413531060., 0.}};
+double b[13] = {14005451./335480064., 0., 0., 0., 0., -59238493./1068277825., 181606767./758867731., 561292985./797845732., -1041891430./1371343529., 760417239./1151165299., 118820643./751138087., -528747749./2220607170., 1./4.};
+double b_[13] = {13451932./455176623., 0., 0., 0., 0., -808719846./976000145., 1757004468./5645159321., 656045339./265891186., -3867574721./1518517206., 465885868./322736535., 53011238./667516719., 2./45., 0.};
+
+typedef struct 
+{
+    double* data;
+    int alloc;
+    int used;
+}vector;
+
+void init(vector* v, int n);
+void reall(vector* v);
+void push_back(vector* v, double val);
+void free_vec(vector* v);
+
+double max(double x1, double x2);
+double min(double x1, double x2);
+double fmax(double x1, double x2);
+double fmin(double x1, double x2);
+
+double f1(double t, double x1, double x2, double p1, double p2, double integr);
+double f2(double t, double x1, double x2, double p1, double p2, double integr);
+double f3(double t, double x1, double x2, double p1, double p2, double integr);
+double f4(double t, double x1, double x2, double p1, double p2, double integr);
+double f5(double t, double x1, double x2, double p1, double p2, double integr);
+
+double check_sol(double t);
+
+double norm(double x1, double x2, double p1, double p2, double integr);
+double get_h(double h, double err, double tol);
+
+
+double dorman_prince(double h, double t,  double x1_ic, double x2_ic, double p1_ic, double p2_ic, double integr_ic, double* x1, double* x2, double* p1, double* p2, double* integr);
+double solve_dp(double l, double r, double tol, double x1_ic, double x2_ic, double p1_ic, double p2_ic, double integr_ic, double* x1, double* x2, double* p1, double* p2, unsigned int attr, const char* name);
+
+
+
+
+
+
+
+int main(void)
+{
+    return 0;
+}
+
+double max(double x1, double x2)
+{
+    return (x1 > x2) ? x1 : x2;
+}
+double min(double x1, double x2)
+{
+    return (x1 > x2) ? x2 : x1;
+}
+double fmax(double x1, double x2)
+{
+    return max(fabs(x1), fabs(x2));
+}
+double fmin(double x1, double x2)
+{
+    return min(fabs(x1), fabs(x2));
+}
+double f1(double t, double x1, double x2, double p1, double p2, double integr)
+{
+    return x2;
+}
+double f2(double t, double x1, double x2, double p1, double p2, double integr)
+{
+    return p2/2. - x1*exp(-alpha*x1);
+}
+double f3(double t, double x1, double x2, double p1, double p2, double integr)
+{
+    return -p2*(-exp(-alpha*x1) + alpha*x1*exp(-alpha*x1));
+}
+double f4(double t, double x1, double x2, double p1, double p2, double integr)
+{
+    return -p1;
+}
+double f5(double t, double x1, double x2, double p1, double p2, double integr)
+{
+    return p2*p2/4.;
+}
+double check_sol(double t)
+{
+    return t;
+}
+double norm(double x1, double x2, double p1, double p2, double integr)
+{
+    return fmax(x1, fmax(x2, fmax(p1, fmax(p2, integr))));
+}
+double get_h(double h, double err, double tol)
+{
+    if(err < EPS)
+        err = 2*EPS;
+    h *= min(facmax, max(facmin, fac*pow(tol/err, 1./7.)));
+    if(h < EPS)
+        h = 2*EPS;
+    return h;
+}
+double dorman_prince(double h, double t,  double x1_ic, double x2_ic, double p1_ic, double p2_ic, double integr_ic, double* x1, double* x2, double* p1, double* p2, double* integr)
+{
+    double tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr;
+    double kx1[13];
+    double kx2[13];
+    double kp1[13];
+    double kp2[13];
+    double kintegr[13];
+    double x1_8, x2_8, p1_8, p2_8, integr_8;
+    double x1_7, x2_7, p1_7, p2_7, integr_7;
+    int i, j;
+    kx1[0] = f1(t, x1_ic, x2_ic, p1_ic, p2_ic, integr_ic);
+    kx2[0] = f2(t, x1_ic, x2_ic, p1_ic, p2_ic, integr_ic);
+    kp1[0] = f3(t, x1_ic, x2_ic, p1_ic, p2_ic, integr_ic);
+    kp2[0] = f4(t, x1_ic, x2_ic, p1_ic, p2_ic, integr_ic);
+    kintegr[0] = f5(t, x1_ic, x2_ic, p1_ic, p2_ic, integr_ic);
+    for(i = 0; i < 13; i++)
+    {
+        tmpx1 = x1_ic;
+        tmpx2 = x2_ic;
+        tmpp1 = p1_ic;
+        tmpp2 = p2_ic;
+        tmpintegr = integr_ic;
+        tmpt = t + c[i]*h;
+        for(j = 0; j < i; j++)
+        {
+            tmpx1 += a[i][j]*h*kx1[j];
+            tmpx2 += a[i][j]*h*kx2[j];
+            tmpp1 += a[i][j]*h*kp1[j];
+            tmpp2 += a[i][j]*h*kp2[j];
+            tmpintegr += a[i][j]*h*kintegr[j];
+        }
+        kx1[i] = f1(tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr);
+        kx2[i] = f2(tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr);
+        kp1[i] = f3(tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr);
+        kp2[i] = f4(tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr);
+        kintegr[i] = f5(tmpt, tmpx1, tmpx2, tmpp1, tmpp2, tmpintegr);
+    }
+    x1_8 = x2_8 = p1_8 = p2_8 = integr_8 = 0;
+    for(i = 0; i < 13; i++)
+    {
+        x1_8 += b[i]*kx1[i];
+        x2_8 += b[i]*kx2[i];
+        p1_8 += b[i]*kp1[i];
+        p2_8 += b[i]*kp2[i];
+        integr_8 += b[i]*kintegr[i];
+    }
+    x1_8 *= h;
+    x2_8 *= h;
+    p1_8 *= h;
+    p2_8 *= h;
+    integr_8 *= h;
+    x1_8 += x1_ic;
+    x2_8 += x2_ic;
+    p1_8 += p1_ic;
+    p2_8 += p2_ic;
+    integr_8 += integr_ic;
+    x1_7 = x2_7 = p1_7 = p2_7 = integr_7 = 0;
+    for(i = 0; i < 13; i++)
+    {
+        x1_7 += b_[i]*kx1[i];
+        x2_7 += b_[i]*kx2[i];
+        p1_7 += b_[i]*kp1[i];
+        p2_7 += b_[i]*kp2[i];
+        integr_7 += b_[i]*kintegr[i];
+    }
+    x1_7 *= h;
+    x2_7 *= h;
+    p1_7 *= h;
+    p2_7 *= h;
+    integr_7 *= h;tmpx1 = tmpx2 = tmpp1 = tmpp2 = tmpintegr = 0;
+    x1_7 += x1_ic;
+    x2_7 += x2_ic;
+    p1_7 += p1_ic;
+    p2_7 += p2_ic;
+    integr_7 += integr_ic;
+    *x1 = x1_7;
+    *x2 = x2_7;
+    *p1 = p1_7;
+    *p2 = p2_7;
+    *integr = integr_7;
+    return norm(x1_7 - x1_8, x2_7 - x2_8, p1_7 - p1_8, p2_7 - p2_8, integr_7 - integr_8);  
+}
+
+/*
+double fmax(double * data, int len)
+{
+    double max;
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        max = (max > fabs(data[i])) ? max : data[i];
+    }
+    return max;
+}
+double fmin(double * data, int len)
+{
+    double min;
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        min = (min <  fabs(data[i])) ? min : data[i];
+    }
+    return min;
+}
+double f1(double * data)
+{
+    return data[X2];
+}
+double f2(double * data)
+{
+    return data[P2]/2 - data[X1]*exp(-alpha*data[X1]);
+}
+double f3(double * data)
+{
+    return -data[P2]*(-exp(-data[X1]*alpha) + alpha*data[X1]*exp(-alpha*data[X1]));
+}
+double f4(double * data)
+{
+    return -data[P1];
+}
+double f5(double * data)
+{
+    return data[P2]*data[P2]/4;
+}
+
+double check_sol(double t)
+{
+    return t;
+}
+void print_data(double * data, FILE * output)
+{
+    fprintf(output, "%e %e %e %e %e \n", data[T], data[X1], data[X2], data[P1], data[P2], data[INTEGR]);
+}
+
+double norm(double * data, int len)
+{
+    return fmax(data, len);
+}
+double norm1(double x1, double x2, double x3, double x4, double x5)
+{
+    return max(fabs(x1), max(fabs(x2), max(fabs(x3), max(fabs(x4), fabs(x5)))));
+}
+double get_h(double h, double err, double tol)
+{
+    if(err < EPS)
+        err = 2*EPS;
+    h *= min(facmax, max(facmin, fac*pow(tol/err, 1./7.)));
+    if(h < EPS)
+        h = 2*EPS;
+    return h;
+}
+void dorman_prince(double h, double * initial_cond, double * res)
+{
+    double tmp[6];
+    double k[5][13];
+    int i, j;
+    k[X1][0] = f1(initial_cond);
+    k[X2][0] = f2(initial_cond);
+    k[P1][0] = f3(initial_cond);
+    k[P2][0] = f4(initial_cond);
+    k[INTEGR][0] = f5(initial_cond);
+    for(i = 1; i < 13; i++)
+    {
+        tmp[T] = initial_cond[T]*c[i]*h;
+        tmp[X1] = initial_cond[X1];
+        tmp[X2] = initial_cond[X2];
+        tmp[P1] = initial_cond[P1];
+        tmp[P2] = initial_cond[P2];
+        tmp[INTEGR] = initial_cond[INTEGR];
+        for(j = 0; j < i; j++)
+        {
+            tmp[X1] += a[i][j]*h*k[X1][j];
+            tmp[X2] += a[i][j]*h*k[X2][j];
+            tmp[P1] += a[i][j]*h*k[P1][j];
+            tmp[P2] += a[i][j]*h*k[P2][j];
+            tmp[INTEGR] += a[i][j]*h*k[INTEGR][j];
+        }
+        k[0][i] = f1(tmp);
+        k[1][i] = f2(tmp);
+        k[2][i] = f3(tmp);
+        k[3][i] = f4(tmp);
+        k[4][i] = f5(tmp);
+    }
+
+    memset(tmp, 0, sizeof(double)*6);
+    for(i = 0; i < 13; i++)
+    {
+        tmp[X1] += b[i]*k[X1][i];
+        tmp[X2] += b[i]*k[X2][i];
+        tmp[P1] += b[i]*k[P1][i];
+        tmp[P2] += b[i]*k[P2][i];
+        tmp[INTEGR] += b[i]*k[INTEGR][i];
+    }
+    tmp[X1] *= h;
+    tmp[X2] *= h;
+    tmp[P1] *= h;
+    tmp[P2] *= h;
+    tmp[INTEGR] *= h;
+    tmp[X1] += initial_cond[X1];
+    tmp[X2] += initial_cond[X2];
+    tmp[P1] += initial_cond[P1];
+    tmp[P2] += initial_cond[P2];
+    tmp[INTEGR] += initial_cond[INTEGR];
+    res[X1] = tmp[X1];
+    res[X2] = tmp[X2];
+    res[P1] = tmp[P1];
+    res[P2] = tmp[P2];
+    res[INTEGR] = tmp[INTEGR];
+
+    memset(tmp, 0, sizeof(double)*6);
+    for(i = 0; i < 13; i++)
+    {
+        tmp[X1] += b_[i]*k[X1][i];
+        tmp[X2] += b_[i]*k[X2][i];
+        tmp[P1] += b_[i]*k[P1][i];
+        tmp[P2] += b_[i]*k[P2][i];
+        tmp[INTEGR] += b_[i]*k[INTEGR][i];
+    }
+    tmp[X1] *= h;
+    tmp[X2] *= h;
+    tmp[P1] *= h;
+    tmp[P2] *= h;
+    tmp[INTEGR] *= h;
+    tmp[X1] += initial_cond[X1];
+    tmp[X2] += initial_cond[X2];
+    tmp[P1] += initial_cond[P1];
+    tmp[P2] += initial_cond[P2];
+    tmp[INTEGR] += initial_cond[INTEGR];
+    res[X1 + 6] = tmp[X1];
+    res[X2 + 6] = tmp[X2];
+    res[P1 + 6] = tmp[P1];
+    res[P2 + 6] = tmp[P2];
+    res[INTEGR + 6] = tmp[INTEGR];
+}
+double solve_dp(double l, double r, double * initital_cond, double * res, unsigned int attr, const char* name)
+{
+    double h, err;
+    double glob_err = 0.;
+    FILE * output;
+    double last = 0.;
+    double max_norm = 0.;
+    double res_tmp[12];
+    double cur[6];
+    memcpy(cur, initital_cond, sizeof(double)*6);
+    h = 0.01;
+    if(!(attr & NO_WRITE))
+    {
+        output = fopen(name, "w");
+        if(output == NULL)
+        {
+            printf("Cannot open file to write \n");
+            exit(EXIT_FAILURE);
+        }
+        print_data(initital_cond, output);
+    }
+    while(l < r)
+    {
+        if(l + h > r)
+            h = r - l;
+        dorman_prince(h, cur, res_tmp);
+        err = norm1(res_tmp[X1] - res_tmp[X1 + 6], res_tmp[X2] - res_tmp[X2 + 6], res_tmp[P1] - res_tmp[P1 + 6], res_tmp[P2] - res_tmp[P2 + 6], res_tmp[INTEGR] - res_tmp[INTEGR + 6]);
+        if(err < tol)
+        {
+            l += h;
+            memcpy(cur, res_tmp + )
+        }
+     }
+}
+*/
---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+enum test
+{
+    LOL,
+    LOL1
+};
+int main()
+{
+    double test1, test2, test3;
+    test1 = test2 = test3 = 0;
+    return 0;
+}
